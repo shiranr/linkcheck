8b10179ff72571d253ae48e9194a034cf6624610
Shiran Rubin Manaev <shiranrubin@microsoft.com>
2022-12-20 14:25:50 +0200

    fix test

--- main_test.go
+++ main_test.go
@@ -8,13 +8,13 @@ import (
 
 func TestLoadConfiguration(t *testing.T) {
 	loadConfiguration()
-	assert.NotEmpty(t, viper.GetDuration("exclude_links"))
+	assert.NotEmpty(t, viper.GetStringSlice("exclude_links"))
 }
 
 func TestHandlerFile(t *testing.T) {
 	loadConfiguration()
 	files := extractReadmeFiles()
 	assert.Len(t, files, 2)
-	assert.Equal(t, files[0], "/shiranr/linkcheck/README.md")
+	assert.Contains(t, files[0], "/shiranr/linkcheck/README.md")
 	assert.Contains(t, files[1], "/shiranr/linkcheck/tests/resources/another_markdown.md")
 }
