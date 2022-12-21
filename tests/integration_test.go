be9142d5f5c3c3aaa740c0f95823dd6caa11bd2e
Shiran Rubin Manaev <shiranrubin@microsoft.com>
2022-12-21 17:19:27 +0200

    start adding integration tests

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -1,1 +1,21 @@
 package tests
+
+import (
+	"github.com/shiranr/linkcheck/models"
+	"github.com/shiranr/linkcheck/utils"
+	"github.com/stretchr/testify/assert"
+	"path/filepath"
+	"runtime"
+	"testing"
+)
+
+func TestFiles(t *testing.T) {
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+	configPath := basepath + "/resources/linkcheck.json"
+	utils.LoadConfiguration(configPath)
+	utils.SetUpLogger()
+	readmeFiles := utils.ExtractReadmeFiles()
+	res := models.GetFilesProcessorInstance().Process(readmeFiles)
+	assert.Equal(t, res.Error(), "")
+}
