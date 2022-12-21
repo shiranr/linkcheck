225ae66186f2b5823acca6bad35d683781fa1b35
Shiran Rubin Manaev <shiranrubin@microsoft.com>
2022-12-21 17:20:16 +0200

    start fixing tests

--- main_test.go
+++ main_test.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"github.com/shiranr/linkcheck/utils"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestLoadConfiguration(t *testing.T) {
-	loadConfiguration()
+	utils.LoadConfiguration("configuration/linkcheck.json")
 	assert.NotEmpty(t, viper.GetStringSlice("exclude_links"))
 }
 
 func TestHandlerFile(t *testing.T) {
-	loadConfiguration()
-	files := extractReadmeFiles()
+	utils.LoadConfiguration("configuration/linkcheck.json")
+	files := utils.ExtractReadmeFiles()
 	assert.Len(t, files, 2)
 	assert.Contains(t, files[0], "/shiranr/linkcheck/README.md")
 	assert.Contains(t, files[1], "/shiranr/linkcheck/tests/resources/another_markdown.md")
