0d224629b9129a6108b97e5dcf81dc0a18017f4e
shiranr <shiranrubin@gmail.com>
2023-01-30 17:02:53 +0200

    add tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,17 +1,18 @@
 package utils
 
 import (
+	"errors"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestLoadConfiguration(t *testing.T) {
+func TestLoadConfigurationSuccessfully(t *testing.T) {
 	LoadConfiguration("../configuration/linkcheck.json")
 	assert.True(t, viper.GetBool("only_errors"))
 }
 
-func TestHandlerFile(t *testing.T) {
+func TestExtractMarkdownFilesSuccessfully(t *testing.T) {
 	LoadConfiguration("../configuration/linkcheck.json")
 	viper.Set("project_path", "../")
 	files := ExtractMarkdownFiles()
@@ -19,4 +20,23 @@ func TestHandlerFile(t *testing.T) {
 	assert.Contains(t, files[0], "/shiranr/linkcheck/README.md")
 	assert.Contains(t, files[1], "/shiranr/linkcheck/tests/resources/MARKDOWN.md")
 	assert.Contains(t, files[2], "/shiranr/linkcheck/tests/resources/another_markdown.md")
+}
+
+func TestExtractMarkdownFilesFail(t *testing.T) {
+	LoadConfiguration("../configuration/linkcheck.json")
+	viper.Set("project_path", "")
+	getDir = func() (dir string, err error) {
+		return "", errors.New("BLA BLA BLA")
+	}
+	files := ExtractMarkdownFiles()
+	assert.Len(t, files, 0)
+}
+
+func TestExtractMarkdownFilesFromList(t *testing.T) {
+	LoadConfiguration("../configuration/linkcheck.json")
+	files := []string{"../README.md", "test.jpg"}
+	readmeFiles := ExtractMarkdownFilesFromList(files)
+	assert.Len(t, readmeFiles, 1)
+	assert.Contains(t, readmeFiles[0], "linkcheck/README.md")
+
 }
